65e1fecdc4c4448e523b9012b45bf8389306c746
Денис Мурашов <murashov@zelrun.com>
2022-06-01 21:19:59 +0300

    добавил пару пермишнов но не до конца

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,9 +2,21 @@ from rest_framework import permissions
 
 
 class AdminOnly(permissions.BasePermission):
-    """Permission доступа только для админа."""
-    def has_permission(self, request, view):
+    """
+    Права доступа: только админ.
+    """
+    def has_object_permission(self, request, view, obj):
         return request.user.is_authenticated and request.user.is_admin
 
+
+class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
+    """"
+    Права доступа: Автор отзыва, модератор или администратор.
+    """
     def has_object_permission(self, request, view, obj):
-        return self.has_permission(request, view)
+        return(
+            request.method in permissions.SAFE_METHODS
+            or request.user == obj.author
+            or request.user.is_moderator
+            or request.user.is_admin
+        )
