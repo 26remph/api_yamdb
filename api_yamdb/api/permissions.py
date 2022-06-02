136b9a2ad6ac13203a2fff5378bb9693bebd413d
Денис Мурашов <murashov@zelrun.com>
2022-06-03 00:07:30 +0300

    добавил mixinы добавил пермишн и чутка скорретировал вьюсет юзера

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -1,17 +1,17 @@
 from rest_framework import permissions
 
 
-class AdminOnly(permissions.BasePermission):
+class AdminOnlyPermission(permissions.BasePermission):
     """
     Права доступа: только админ.
     """
     def has_object_permission(self, request, view, obj):
-        return request.user.is_authenticated and request.user.is_admin
+        return (request.user.is_authenticated and request.user.is_admin) or request.user.is_staff
 
 
 class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
     """"
-    Права доступа: Автор отзыва, модератор или администратор.
+    Права доступа: Автор, модератор или администратор.
     """
     def has_object_permission(self, request, view, obj):
         return(
@@ -19,4 +19,17 @@ class AuthorModeratorAdminOrReadOnly(permissions.BasePermission):
             or request.user == obj.author
             or request.user.is_moderator
             or request.user.is_admin
+        )
+
+class AdminOrReadonly(permissions.BasePermission):
+    """
+    Права доступа:
+    чтение для всех
+    изменение только для администратор
+    """
+    def has_permission(self, request, view):
+        return (
+            request.method in permissions.SAFE_METHODS
+            or (request.user.is_authenticated and request.user.is_admin)
+            or request.user.is_staff
         )
